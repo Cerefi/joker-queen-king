e23dcb4614593d6c4663fee5db5d819eb4c81b23
Fercie <ferciecaseria.xix@gmail.com>
2023-11-17 14:38:47 +0800

    Add the round function

--- external.js
+++ external.js
@@ -1,9 +1,43 @@
 const choices = ['joker', 'queen', 'king']
 const jokerBtn = document.querySelector('#joker');
+const queenBtn = document.querySelector('#queen')
+const kingBtn = document.querySelector('#king')
 
-alert(randomChoice());
+
+
 
 function randomChoice() {
     const random = Math.floor(Math.random()*3);
     return choices[random];
+}
+
+function round(player, computer) {
+    if (player === 'joker') {
+        if (computer === 'queen') {
+            computerSCore++;
+        }
+        else if (computer === 'king') {
+            playerScore++;
+        }
+    }
+    else if (player === 'queen') {
+        if (computer === 'joker') {
+            playerScore++;
+        }
+        else if (computer === 'king') {
+            computerScore++
+        }
+    }
+    else {
+        if (computer === 'king') {
+            if (computer === 'joker') {
+                computerScore++;
+            }
+            else if (computer === 'queen') {
+                playerScore++;
+            }
+        }
+    }
+
+    return;
 }
